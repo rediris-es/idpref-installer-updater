7257720deb046bfb904e8301945c5d9e1a178064
adriangmemorandum <adriangomez@memorandum.net>
2018-08-02 17:00:39 +0200

    Documentación y primera versión del código

---
+++ ComposerScript/Installer.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace ComposerScript;
+
+use Composer\Script\Event;
+
+class Installer
+{
+
+    public static function postInstall(Event $event)
+    {
+        self::configureSimpleSAMLphp();
+    }
+
+    public static function postUpdate(Event $event)
+    {
+        self::configureSimpleSAMLphp();
+    }
+
+    private static function configureSimpleSAMLphp()
+    {
+    	if (file_exists('simplesamlphp')) {
+			self::rm_r('simplesamlphp');
+		}
+
+    	shell_exec('composer create-project composer create-project --prefer-dist --stability=dev simplesamlphp/simplesamlphp:dev-Xnew-ui');
+		self::rm_r('vendor');
+		
+		if (!file_exists('simplesamlphp/cert')) {
+			mkdir('simplesamlphp/cert');
+		}
+
+		$apacheUser = exec('ps axo user | grep apache | grep -v root | uniq');
+		$apacheGroup = exec('ps axo group | grep apache | grep -v root | uniq');
+		$filePermissions = octdec("0664");
+		$folderPermissions = octdec("0775");
+
+		copy("simplesamlphp/metadata-templates/saml20-idp-hosted.php", "simplesamlphp/metadata/saml20-idp-hosted.php");
+		copy("simplesamlphp/metadata-templates/saml20-idp-remote.php", "simplesamlphp/metadata/saml20-idp-remote.php");
+		copy("simplesamlphp/metadata-templates/saml20-sp-remote.php", "simplesamlphp/metadata/saml20-sp-remote.php");
+		copy("simplesamlphp/config-templates/acl.php", "simplesamlphp/config/acl.php");
+		copy("simplesamlphp/config-templates/authmemcookie.php", "simplesamlphp/config/authmemcookie.php");
+		copy("simplesamlphp/config-templates/authsources.php", "simplesamlphp/config/authsources.php");
+		copy("simplesamlphp/config-templates/config.php", "simplesamlphp/config/config.php");
+		chmod("simplesamlphp/metadata/saml20-idp-hosted.php", $filePermissions);
+		chmod("simplesamlphp/metadata/saml20-sp-remote.php", $filePermissions);
+		self::copy_r("modules/idpinstaller", "simplesamlphp/modules/idpinstaller");
+		self::copy_r("modules/hubandspoke", "simplesamlphp/modules/hubandspoke");
+		self::copy_r("modules/sir2skin", "simplesamlphp/modules/sir2skin");
+		self::rm_r('modules');
+		self::chmod_r("simplesamlphp/modules", $folderPermissions);
+
+		if (file_exists('simplesamlphp/modules/hubandspoke/default-disable')) {
+			rename('simplesamlphp/modules/hubandspoke/default-disable','simplesamlphp/modules/hubandspoke/default-enable');
+		}
+
+		if (file_exists('simplesamlphp/modules/exampleauth/default-disable')) {
+			unlink('simplesamlphp/modules/exampleauth/default-disable');
+		}
+
+		touch('simplesamlphp/modules/exampleauth/enable');
+		touch('simplesamlphp/modules/sir2skin/default-enable');
+		
+		if (file_exists('simplesamlphp/modules/sir2skin/default-disable')) {
+			rename('simplesamlphp/modules/sir2skin/default-disable','simplesamlphp/modules/sir2skin/default-enable');
+		}
+
+		self::downloadAndWriteConfig();
+		chmod("simplesamlphp/config/config.php", $filePermissions);
+		chmod("simplesamlphp/modules/idpinstaller/lib/makeCert.sh", $folderPermissions);
+
+		if (file_exists('simplesamlphp/modules/sir2skin/default.disable')) {
+			rename('simplesamlphp/modules/sir2skin/default.disable','simplesamlphp/modules/sir2skin/default-enable');
+		}
+		
+		self::chmod_r("simplesamlphp/cert", $folderPermissions);
+		self::chown_r('simplesamlphp', $apacheUser, $apacheGroup);
+    }
+
+    private static function downloadAndWriteConfig()
+    {
+
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL, "http://www.rediris.es/sir2/IdP/install/config.php.txt");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+		$result = curl_exec($ch);
+		
+		curl_close ($ch);
+
+		file_put_contents('simplesamlphp/config/config.php', $result);
+
+    }
+
+    private static function chmod_r($path, $filemode) 
+    {
+	    chmod($path, $filemode);
+
+	    $d = opendir($path);
+
+	    while (($file = readdir($d)) !== false) {
+	        if($file != '.' && $file != '..') {
+		        $typepath = $path.'/'.$file;
+
+		        if (filetype ($typepath) == 'dir') {
+	                self::chmod_r($typepath, $filemode);
+	            }
+	            chmod($typepath, $filemode);
+	        }
+	    }
+
+	    closedir($d);
+
+	}
+
+
+    private static function chown_r($path, $uid, $gid)
+	{
+		chown($path, $uid);
+		chgrp($path, $gid);
+
+	    $d = opendir ($path) ;
+	    
+	    while(($file = readdir($d)) !== false) {
+	        if ($file != "." && $file != "..") {
+
+	            $typepath = $path . "/" . $file ;
+
+	            if (filetype ($typepath) == 'dir') {
+	                self::chown_r($typepath, $uid, $gid);
+	            }
+
+	            chown($typepath, $uid);
+	            chgrp($typepath, $gid);
+
+	        }
+	    }
+
+	    closedir($d);
+
+	}
+
+
+    private static function rm_r($src) 
+    {
+	    $dir = opendir($src);
+	    while(false !== ( $file = readdir($dir)) ) {
+	        if (( $file != '.' ) && ( $file != '..' )) {
+	            $full = $src . '/' . $file;
+	            if ( is_dir($full) ) {
+	                self::rm_r($full);
+	            }
+	            else {
+	                unlink($full);
+	            }
+	        }
+	    }
+	    closedir($dir);
+	    rmdir($src);
+	}
+
+
+	private static function copy_r($src,$dst) 
+	{
+	    $dir = opendir($src);
+	    @mkdir($dst);
+	    while(false !== ( $file = readdir($dir)) ) {
+	        if (( $file != '.' ) && ( $file != '..' )) {
+	            if ( is_dir($src . '/' . $file) ) {
+	                self::copy_r($src . '/' . $file,$dst . '/' . $file);
+	            }
+	            else {
+	                copy($src . '/' . $file,$dst . '/' . $file);
+	            }
+	        }
+	    }
+	    closedir($dir);
+	}
+}
+
+?>
--- README.md
+++ README.md
@@ -4,6 +4,26 @@ This project is used to bootstrap a new IdP installation (or, in the future, upd
 
 The project aims to have all the needed dependencies correctly configured.
 
+# Dependencies
+
+The installation of IdP requires composer.
+
+Composer can be downloaded running this command:
+
+`curl -sS https://getcomposer.org/installer | php`
+
+Finally, it can be configured globally to have the composer command in any directory on your computer.
+
+`mv composer.phar /usr/local/bin/composer`
+
 # Usage
 
-Usage instructions to be added here soon...
+In order to produce an initial installation, execute the following command:
+
+`composer install`
+
+In your current directory a simplesamlphp directory will be created with the initial configuration to run the IdP installer within.
+
+In order to update all packages and dependencies up to the latest stable version, execute the following command:
+ 
+`composer update`
