c6859a857b77ab7841c53d11dea3523f7355cee3
adriangmemorandum <adriangomez@memorandum.net>
2018-10-31 11:05:55 +0100

    Prueba

--- ComposerScript/Installer.php
+++ ComposerScript/Installer.php
@@ -33,11 +33,15 @@ class Installer
 		/*rename('./vendor/simplesamlphp/simplesamlphp','./simplesamlphp');
 		rename('./vendor','./simplesamlphp/vendor');*/
 		
-		self::copy_r("./vendor/simplesamlphp/simplesamlphp", "./simplesamlphp/");
-		
+		//self::copy_r("./vendor/simplesamlphp/simplesamlphp", "./simplesamlphp/");
+
+
+
 		if (!file_exists('simplesamlphp')) {
 			mkdir('simplesamlphp');
 		}
+
+		exec('\cp -r ./vendor/simplesamlphp/simplesamlphp/* ./simplesamlphp');
 
 		if (!file_exists('simplesamlphp/cert')) {
 			mkdir('simplesamlphp/cert');
